59f3058706dcbcfb0e50a6116cb13adbe4e950c1
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2018-06-21 22:29:53 +0900

    0,3Bはダメだけど後は大丈夫そう

--- cnt.py
+++ cnt.py
@@ -11,6 +11,7 @@ with open("01_01_201806180000_5_SUiRU/2018061800000101VM.cnt","rb") as f:
 
     print(hex(start_date),frame_timespan,datablock_length)
 
+    j=0
     while True:
         try:
             organization_id,organization_net_id,channel_id,sample_size_and_number,first_data=struct.unpack(">BBHHi",f.read(1+1+2+2+4))
@@ -18,12 +19,19 @@ with open("01_01_201806180000_5_SUiRU/2018061800000101VM.cnt","rb") as f:
             sample_size=(sample_size_and_number>>12) & 0xF
             sample_number=sample_size_and_number & 0xFFF
             print(organization_id,organization_net_id,channel_id,sample_size,sample_number,first_data)
-            if sample_size!=1:
-                print("だめ．差分が1Byteじゃない",sample_size)
+            if sample_size in [0,3]:
+                print("だめ．差分",sample_size)
             data=[first_data]
             for i in range(sample_number-1):
-                data.append(data[i]+int(struct.unpack(">b",f.read(1))[0]))
+                #FIXME:.5Byte 3Byte
+                current_data=int(struct.unpack(">"+["b","b","h","i","i"][sample_size],f.read(sample_size))[0])
+                #print(current_data)
+                #input("?")
+                data.append(data[i]+current_data)
             plt.plot(data)
+            #input("ok?")
+            j+=1
+            print(j)
         except Exception:
-            pass
+            break
     plt.show()
